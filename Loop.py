fea5997c11f7e4a451f98e78d26b6eccf6649cb7
Moacir Domingos <59853005+mdsjr@users.noreply.github.com>
2025-05-09 12:18:20 -0300

    Create Loop.py

---
+++ Loop.py
@@ -0,0 +1,13 @@
+# Definindo a variável INDICE com o valor 13
+# Iniciando SOMA e K com 0
+INDICE = 13
+SOMA = 0
+K = 0
+
+# Loop para calcular a soma dos números de 1 até INDICE
+while K < INDICE:
+    K = K + 1
+    SOMA = SOMA + K
+
+# Imprimindo o valor final de SOMA
+print(SOMA)
